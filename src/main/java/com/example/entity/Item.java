a6906de09c336b0cebca4d7f9b921302e4c3d50f
yuki-git01 <nn4d8303264.7r@gmail.com>
2024-05-29 21:57:51 +0900

    Categoryの追加

--- src/main/java/com/example/entity/Item.java
+++ src/main/java/com/example/entity/Item.java
@@ -7,54 +7,51 @@ public class Item {
 	private String name;
 	private Integer price;
 	private LocalDateTime deletedAt;
-	private Integer categoryId;    
-    private Integer stock;
-	
-    public Integer getId() {
-    	return this.id;
-    }
-    
-    public void setId(Integer id) {
-    	this.id = id;
-    }
-    
-    public String getName() {
-        return this.name;
-    }
+	private Category category;
+	private Integer stock;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public Integer getId() {
+		return this.id;
+	}
 
-    public Integer getPrice() {
-        return this.price;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-    
-    public LocalDateTime getDeletedAt() {
-    	return this.deletedAt;
-    }
-    
-    public void setDeletedAt(LocalDateTime deletedAt) {
-    	this.deletedAt = deletedAt;
-    }
+	public String getName() {
+		return this.name;
+	}
 
-    public Integer getCategoryId() {
-    	return this.categoryId;
-    }
-    
-    public void setCategoryId(Integer categoryId) {
-    	this.categoryId = categoryId;
-    }
-    
-    public void setStock(Integer stock) {
-    	this.stock = stock;
-    }
-    
-    public Integer getStock() {
-    	return this.stock;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public LocalDateTime getDeletedAt() {
+		return this.deletedAt;
+	}
+
+	public void setDeletedAt(LocalDateTime deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public Integer getStock() {
+		return this.stock;
+	}
+
+	public Category getCategory() {
+		return this.category;
+	}
+
 }
